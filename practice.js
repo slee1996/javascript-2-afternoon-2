92dcd479261ebdbd8a44371fec92ae32252d23f0
Spencer Lee <spencerlee96@gmail.com>
2019-11-01 11:46:16 -0600

    javascript 2 afternoon 2: electric boogaloo

--- practice.js
+++ practice.js
@@ -19,6 +19,9 @@ var arr = [10,20,30];
 
 //Code Here
 
+function first(arr) {
+  return arr.shift(0)
+}
 
 
 ////////// PROBLEM 2 //////////
@@ -34,7 +37,9 @@ var arr = [40,50,60];
 
 //Code Here
 
-
+function last(arr) {
+  return arr.pop()
+}
 
 ////////// PROBLEM 3 //////////
 
@@ -49,7 +54,11 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
 
 //Code Here
 
-
+function looper(family) {
+  for (i = 0; i < family.length; i++) {
+    alert(family[i])
+  }
+}
 
 ////////// PROBLEM 4 //////////
 
@@ -64,7 +73,11 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 
 //Code Here
 
-
+function reversedLooper(letters) {
+  for (i = letters.length -1; i >= 0; i--) {
+    alert(letters[i])
+  }
+}
 
 ////////// PROBLEM 5 //////////
 
@@ -79,11 +92,15 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
 
 //Code Here
 
-
-
-
-
-
+function evenFinder(nums) {
+  var evens = []
+  for (i = 0; i < nums.length; i++){
+    if (nums[i] % 2 === 0) {
+      evens.push(nums[i])
+    }
+  }
+  return evens
+}
 
 /////////////////////// EXTRA PRACTICE PROBLEMS BELOW ////////////////////
 ////////// MOVE ONTO NEXT SECTION BEFORE WORKING ON THESE ////////////////
@@ -108,7 +125,15 @@ var numbersArray = [1,2,34,54,55,34,32,11,19,17,54,66,13];
 
 //Code Here
 
-
+function divider(numbersArray){
+  evens = numbersArray.filter(function(number){
+     return number % 2 === 0;
+ });
+  odds = numbersArray.filter(function(number){
+     return number % 2 !== 0;
+  });
+  return [evens,odds];
+ }
 
 ////////// PROBLEM 7 //////////
 
@@ -130,6 +155,10 @@ var getRandomArbitrary = function() {
 
 //Code Here
 
+function finder(arr) {
+  let randomNumber = getRandomArbitrary()
+  return arr.indexOf(randomNumber) === -1 ? false: true
+}
 
 
 ////////// PROBLEM 8 //////////
@@ -159,6 +188,9 @@ var myGroceryList = ['chips', 'pizza', 'hotpockets', 'MtnDew', 'corndogs'];
 
 //Code Here
 
+function removeItem(myGroceryList, groceryItem) {
+
+}
 
 
 ////////// PROBLEM 9 //////////
